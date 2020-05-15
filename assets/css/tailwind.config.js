2bf849c7b068e091a3eb4093c63c5439eaf7d0e6
Bob Chen <chenbobby168@gmail.com>
2020-05-14 23:18:29 -0700

    Adds more styling for header.

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        blue: "#12c2e9",
+        purple: "#c471ed",
+        orange: "#f64f59",
+        gray: {
+          light: "#edf2f7",
+          default: "#4f575f",
+          dark: "#1e2123",
+        }
+      }
+    }
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive']
+  },
   plugins: []
 }
